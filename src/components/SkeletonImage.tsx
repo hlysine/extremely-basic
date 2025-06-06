00d182c6c20e0bde52a203439e16e4e134bf3f3f
Henry Lin <henry.ys.lin@gmail.com>
2025-06-06 21:49:02 +0800

    Show skeleton during image load

---
+++ src/components/SkeletonImage.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { cn } from '../utils/uiUtils';
+
+export interface SkeletonImageProps {
+  title?: string;
+  className?: string;
+  alt?: string;
+  src?: string;
+}
+
+// million-ignore
+export default function SkeletonImage({
+  title,
+  className,
+  alt,
+  src,
+  ...rest
+}: SkeletonImageProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      <div
+        className={cn('skeleton w-full h-64', className, isLoading || 'hidden')}
+      ></div>
+      <img
+        className={cn(className, isLoading && 'hidden')}
+        loading="eager"
+        src={src}
+        alt={alt}
+        title={title}
+        onLoad={() => setIsLoading(false)}
+        onError={() => setIsLoading(false)}
+        {...rest}
+      />
+    </>
+  );
+}
--- src/components/StyledMarkdown.tsx
+++ src/components/StyledMarkdown.tsx
@@ -1,11 +1,23 @@
 import { memo } from 'react';
 import Markdown from 'markdown-to-jsx';
 import './styledMarkdown.css';
+import SkeletonImage from './SkeletonImage';
 
 export interface MarkdownProps {
   children: string;
 }
 
 export default memo(function StyledMarkdown({ children }: MarkdownProps) {
-  return <Markdown className="markdown">{children}</Markdown>;
+  return (
+    <Markdown
+      className="markdown"
+      options={{
+        overrides: {
+          img: SkeletonImage,
+        },
+      }}
+    >
+      {children}
+    </Markdown>
+  );
 });
